60b02c8f7a1b98dad6fb5f7dc5a99cfab1956740
Gabriel Valladares <gabrielvalladares1@gmail.com>
2020-06-11 20:15:59 -0400

    picture randomly picked from assets

--- Angel Cards/ViewController.swift
+++ Angel Cards/ViewController.swift
@@ -12,9 +12,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var angelCardImage: UIImageView!
     
-    var angelCard = #imageLiteral(resourceName: "make a wish")
-    
-    let angelCardList = [ #imageLiteral(resourceName: "Ask for a sign (1)"), #imageLiteral(resourceName: "Love and Gratitude"), #imageLiteral(resourceName: "Flow with it"), #imageLiteral(resourceName: "youre not alone image"), #imageLiteral(resourceName: "Precious Time"), #imageLiteral(resourceName: "Awakening"), #imageLiteral(resourceName: "pause and reflect"), #imageLiteral(resourceName: "Lean on Me (1)"), #imageLiteral(resourceName: "Let Your Creativity Flow"), #imageLiteral(resourceName: "Believe in Miracles"), #imageLiteral(resourceName: "make a wish"), #imageLiteral(resourceName: "Let love in"), #imageLiteral(resourceName: "you_ve got this"), #imageLiteral(resourceName: "Healing"), #imageLiteral(resourceName: "spread your wings"), #imageLiteral(resourceName: "sootheyoursoul"), #imageLiteral(resourceName: "Sensitive Soul"), #imageLiteral(resourceName: "Angels of Peace"), #imageLiteral(resourceName: "Embrace the new") ]
+    var angelCard = #imageLiteral(resourceName: "12")
     
     let angelCardBackGround = #imageLiteral(resourceName: "angelcardback")
     
@@ -22,7 +20,7 @@ class ViewController: UIViewController {
     // Displays back of card and changes the value of angelCard
     @IBAction func pickANewCard(_ sender: UIButton) {
         angelCardImage.image = angelCardBackGround
-        angelCard = angelCardList.randomElement()!
+        angelCard = UIImage(named: "\(arc4random_uniform(19) + 1)")!
     }
     
     // Will flip from the back of the card to the front
